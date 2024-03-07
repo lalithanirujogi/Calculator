cdd1b016d1a7a9056ac71a873cda148712489f1f
lalithanirujogi <162543055+lalithanirujogi@users.noreply.github.com>
2024-03-07 11:23:09 +0530

    calculator.py

---
+++ calculator.py
@@ -0,0 +1,26 @@
+print("Choose an operator to perform:")
+print("1.ADDITION")
+print("2.SUBTRACTION")
+print("3.MULTIPLICATION")
+print("4.DIVISION")
+
+operation = input()
+
+if operation == "1":
+    a = float(input("Enter first number: "))
+    b = float(input("Enter second number: "))
+    print("The sum is " +str(a + b))
+elif operation == "2":
+    a = float(input("Enter first number: "))
+    b = float(input("Enter second number: "))
+    print("The sum is " + str(a - b))
+elif operation == "3":
+     a = float(input("Enter first number: "))
+     b = float(input("Enter second number: "))
+     print("The sum is " + str(a * b))
+elif operation == "4":
+    a = float(input("Enter first number: "))
+    b = float(input("Enter second number: "))
+    print("The sum is " + str (a / b))
+else:
+    print("Invalid ")
